0dbfd78ff3619bdd0e166e3f51309a270a308991
Irwan Shahiman <irwanshahiman4@gmail.com>
2026-08-04 16:47:41 +0800

    commit LabActivity1_1

---
+++ LabActivity1_1/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    public static void main (String[] args) {
+        
+        System.out.println("Student name: Ali Ahmad");
+        System.out.println("Registration Number: DIT12345");
+        System.out.println("Student Class: DIT3A");
+        System.out.println();
+        
+        System.out.println("Student name: Siti Najwa binti Abdul");
+        System.out.println("Registration Number: DIT67890");
+        System.out.println("Student Class: DIT3A");
+        System.out.println();
+        
+        System.out.println("Student name: Chia Hua Jin");
+        System.out.println("Registration Number: DIT45234");
+        System.out.println("Student Class: DIT3A");
+    }
+} 
