f1a6c89fe006daf565b3b2a30f4b92185a5e41ee
W4NNNNNNNNNNNN <irwanshahiman4@gmail.com>
2026-08-08 21:21:22 +0800

    upload exercise-lab-activity1

---
+++ Exercise1_1/Laptop.java
@@ -0,0 +1,11 @@
+public class Laptop{
+    public String brand;
+    public String model;
+    public double price;
+    
+    public void displayInfo() {
+        System.out.println("Laptop Brand: " + brand);
+        System.out.println("Laptop Model: " + model);
+        System.out.println("Laptop Price: RM" + price);
+    }
+} 
---
+++ Exercise1_1/MainLaptop.java
@@ -0,0 +1,12 @@
+public class MainLaptop {
+    
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop();
+        
+        laptop.brand= "Dell";
+        laptop.model = "Inspiron";
+        laptop.price = 3200.0;
+        
+        laptop.displayInfo();
+    }
+} 
