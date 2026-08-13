29528673adfb7a311cb557365d4365195e05dc2d
W4NNNNNNNNNNNN <irwanshahiman4@gmail.com>
2026-08-13 20:51:16 +0800

    apload LabActivity 1_2

---
+++ LabActivity1_2/Cat.java
@@ -0,0 +1,15 @@
+package LabActivity1_2;
+
+
+// Cat class
+public class Cat {
+    // Fields Attribute
+    String name;
+    String breed;
+    int age;
+    
+    // Method (behavior)
+    public void meow() {
+        System.out.println(name + " says Meow!");
+    }
+}
---
+++ LabActivity1_2/MainCat.java
@@ -0,0 +1,25 @@
+package LabActivity1_2;
+
+
+// MainCat class
+public class MainCat {
+    public static void main(String[] args) {
+        //Create an object (instance) of tje Cat class
+        Cat myCat = new Cat();
+        
+        // Access and set the object's fields
+        myCat.name = "Whiskers";
+        myCat.breed = "Persian";
+        myCat.age = 2;
+        
+        // Call the object's method
+        myCat.meow();
+        
+        // Create another Cat object
+        Cat anotherCat = new Cat();
+        anotherCat.name = "Milo";
+        anotherCat.breed = "Siamese";
+        anotherCat.age = 4;
+        anotherCat.meow();
+    }
+}
